f3265c1079f2fd4d9ef163f7de6cf91a3fcc543c
pybash1 <example@example.com>
2022-03-28 10:24:39 +0530

    update

--- src/App.svelte
+++ src/App.svelte
@@ -20,19 +20,131 @@
 			<img src="https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=60" class="max-w-sm rounded-lg shadow-2xl" alt="" />
 			<div>
 			<h1 class="text-5xl font-bold">v4 Just Released!</h1>
-			<p class="py-6">CutCode 4 is now out with Feature #1, Feature #2, and much more small features!</p>
-			<button class="btn btn-primary">Download Now</button>
+			<p class="py-6">CutCode 4 is now out with Feature #1, Feature #2, and much more small features! By the way these images are placeholders replace them with actual ones!</p>
+			<button class="btn btn-primary normal-case">Download Now</button>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="divider px-28"></div>
-<div class="text-5xl font-bold px-24 flex flex-col items-center">
+<div class="text-5xl font-bold px-28 flex flex-col items-center">
 	<div class="text-primary">
 		CutCode - Boost Your Productivity
 	</div>
 	&nbsp;
-	<div class="font-normal text-lg">
-		CutCode is an open source tool made for developers to boost their productivity and save time by helping them save essential code as code snippets in safe place. These code snippets can be searched and accessed at anytime. It is a very helpful tool as you no longer need to remember your solutions and where you originally found them.
+	<div class="font-normal text-lg justify-between flex">
+		<div class="w-1/2">CutCode is an open source tool made for developers to boost their productivity and save time by helping them save essential code as code snippets in safe place. These code snippets can be searched and accessed at anytime. It is a very helpful tool as you no longer need to remember your solutions and where you originally found them.</div>
+		<div><img src="https://api.lorem.space/image/movie?w=300&h=150" class="max-w-sm rounded-box" alt="" /></div>
+	</div>
+	<div class="divider px-14"></div>
+	<div class="">
+		<h1 class="text-primary">Features</h1>
+	</div>
+	&nbsp;
+	<div>
+		<p class="text-center text-lg font-normal">Get an overview of the features of CutCode here! @Abdesol add more data here. Lorem ipsum dolor sit ametconsectetur adipisicing elit. Commodi a iusto fugit quaerat optio nostrum corrupti est possimus maiores voluptas, ipsam architecto!</p>
+	</div>
+	&nbsp;
+	<div class="flex flex-row items-center">
+		<div class="flex flex-col items-center">
+			<img class="mask mask-circle" src="https://api.lorem.space/image/book?w=160&h=160" alt="" />
+			<h4 class="text-2xl">Powerful Syntax Highlighting</h4>
+			<p class="text-lg font-normal text-center">With CutCode's powerful syntax highlighting, never fail to recognize the language of a snippet. @Abdesol gimme more data please.</p>
+		</div>
+		<div>&nbsp;&nbsp;&nbsp;</div>
+		<div class="flex flex-col items-center">
+			<img class="mask mask-circle" src="https://api.lorem.space/image/book?w=160&h=160" alt="" />
+			<h4 class="text-2xl">Cross-Platform Support</h4>
+			<p class="text-lg font-normal text-center">CutCode with its latest release is now available on Windows, MacOS(Intel & M1), and Linux!</p>
+		</div>
+		<div>&nbsp;&nbsp;&nbsp;</div>
+		<div class="flex flex-col items-center">
+			<img class="mask mask-circle" src="https://api.lorem.space/image/book?w=160&h=160" alt="" />
+			<h4 class="text-2xl">Sync Once! Use Anywhere!</h4>
+			<p class="text-lg font-normal text-center">CutCode's powerful offline and online sync capabilities allow you to save your snippets once and use them anywhere, anytime without hassle.</p>
+		</div>
+	</div>
+	<div class="divider px-14"></div>
+	<div class="">
+		<h1 class="text-primary">About</h1>
+	</div>
+	&nbsp;
+	<div class="text-lg font-normal flex flex-col items-center">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-500 mb-8" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg>
+		<p class="text-center px-32">As a developer, we often search on Google / StackOverFlow for doubts, issues related to our code and our projects, when programming. As a result, we visit a lot of different posts, even different websites to find the right solution to our problem, but many times when we see a proper solution to your problem, we tend to forget where we found it or even the solution. Hence, we decided to create a tool that helps us remember and save our solutions as small snippets, so that we never forget about it and access it whenever we need it." and this one here...</p>
+		<span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+		<p class="text-white text-md">CutCode</p>
+		<p class="text-gray-300 text-md">Product</p>
+	</div>
+	<div>&nbsp;&nbsp;</div>
+	<div class="grid grid-cols-2 gap-4 text-lg font-normal">
+		<div class="card min-w-fit bg-base-100 shadow-xl">
+			<div class="card-body">
+			  <h2 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-500 mb-8" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg></h2>
+			  <p>Nice app, thank you @CutCodeDevs for developing such a nice app.</p>
+			  <div class="card-actions justify-end">
+				<div class="flex flex-row items-center">
+					<div class="avatar">
+						<div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+						  <img src="https://api.lorem.space/image/face?hash=3174" />
+						</div>
+					</div>
+					&nbsp;&nbsp;&nbsp;
+					<a class="bg-base-100 text-white">SoulNinja</a>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		<div class="card min-w-fit bg-base-100 shadow-xl">
+			<div class="card-body">
+			  <h2 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-500 mb-8" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg></h2>
+			  <p>Nice app, HI, thank you @CutCodeDevs for developing such a nice app.</p>
+			  <div class="card-actions justify-end">
+				<div class="flex flex-row items-center">
+					<div class="avatar">
+						<div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+						  <img src="https://api.lorem.space/image/face?hash=3174" />
+						</div>
+					</div>
+					&nbsp;&nbsp;&nbsp;
+					<a class="bg-base-100 text-white">PyBash</a>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		<div class="card min-w-fit bg-base-100 shadow-xl">
+			<div class="card-body">
+			  <h2 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-500 mb-8" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg></h2>
+			  <p>The app is nice, overall design looks nice, the syncing also works fine, I don't have any complaints related to the app, changing some colors would be nice.</p>
+			  <div class="card-actions justify-end">
+				<div class="flex flex-row items-center">
+					<div class="avatar">
+						<div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+						  <img src="https://api.lorem.space/image/face?hash=3174" />
+						</div>
+					</div>
+					&nbsp;&nbsp;&nbsp;
+					<a class="bg-base-100 text-white">Abdesol</a>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		<div class="card min-w-fit bg-base-100 shadow-xl">
+			<div class="card-body">
+			  <h2 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-500 mb-8" viewBox="0 0 975.036 975.036"><path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path></svg></h2>
+			  <p>Nice app, thank you @CutCodeDevs for developing such a nice app.</p>
+			  <div class="card-actions justify-end">
+				<div class="flex flex-row items-center">
+					<div class="avatar">
+						<div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+						  <img src="https://api.lorem.space/image/face?hash=3174" />
+						</div>
+					</div>
+					&nbsp;&nbsp;&nbsp;
+					<a class="bg-base-100 text-white">KortaPo</a>
+				</div>
+			  </div>
+			</div>
+		  </div>
 	</div>
 </div>
